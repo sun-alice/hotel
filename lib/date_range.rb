7c968f46c87c1046c80f79e057a111f5669577ee
Alice <alicesunhi@gmail.com>
2019-09-02 17:11:29 -0700

    installed scaffolding

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,18 +1,20 @@
 module Hotel
   class DateRange
     attr_accessor :start_date, :end_date
-
+    
     def initialize(start_date, end_date)
+      @start_date = start_date
+      @end_date = end_date
     end
-
+    
     def overlap?(other)
       return false
     end
-
+    
     def include?(date)
       return false
     end
-
+    
     def nights
       return 3
     end
--- test/hotel_controller_spec.rb
+++ test/hotel_controller_spec.rb
@@ -1,4 +1,4 @@
-require_relative "spec_helper"
+require_relative "test_helper"
 
 describe Hotel::HotelController do
   before do
@@ -16,17 +16,17 @@ describe Hotel::HotelController do
       it "takes two Date objects and returns a Reservation" do
         start_date = @date
         end_date = start_date + 3
-
+        
         reservation = @hotel_controller.reserve_room(start_date, end_date)
-
+        
         expect(reservation).must_be_kind_of Hotel::Reservation
       end
     end
-
+    
     describe "reservations" do
       it "takes a Date and returns a list of Reservations" do
         reservation_list = @hotel_controller.reservations(@date)
-
+        
         expect(reservation_list).must_be_kind_of Array
         reservation_list.each do |res|
           res.must_be_kind_of Reservation
@@ -34,15 +34,15 @@ describe Hotel::HotelController do
       end
     end
   end
-
+  
   describe "wave 2" do
     describe "available_rooms" do
       it "takes two dates and returns a list" do
         start_date = @date
         end_date = start_date + 3
-
+        
         room_list = @hotel_controller.available_rooms(start_date, end_date)
-
+        
         expect(room_list).must_be_kind_of Array
       end
     end
